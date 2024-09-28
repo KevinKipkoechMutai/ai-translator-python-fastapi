490727fac0aa2a52b6790cc15209b47c80be9382
kevinmutai <mutaikevinkip.05@gmail.com>
2024-09-28 22:36:51 +0300

    endpoint

--- app/crud.py
+++ app/crud.py
@@ -1,5 +1,18 @@
 from sqlalchemy.orm import Session
+from fastapi import Depends
 import models
+from .database import get_db
 
-def create_translation_task(db: Session, text: str, languages: list):
+def create_translation_task(text: str, languages: list, db: Session = Depends(get_db)):
+    task = models.TranslationTask(text=text, languages=languages)
+    db.add(task)
+    db.commit()
+    db.refresh(task)
+
+    return task
+
+def get_translation_task(db: Session, task_id: int):
+    return db.query(models.TranslationTask)
+
+def perform_translation():
     return ""
