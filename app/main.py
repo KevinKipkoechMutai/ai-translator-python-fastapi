e0a04aba2e5ea80c6aa8b3adc035bfc8232cd0c5
kevinmutai <mutaikevinkip.05@gmail.com>
2024-09-28 22:36:21 +0300

    project layout

--- app/main.py
+++ app/main.py
@@ -3,6 +3,7 @@ from fastapi.responses import HTMLResponse
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.templating import Jinja2Templates
 import schemas
+import crud
 
 app = FastAPI()
 
@@ -22,4 +23,7 @@ def index(request: Request):
 
 @app.post("/translate", response_model=schemas.TaskResponse)
 def translate(request: schemas.TranslationRequest):
-    return ""
+    task = crud.create_translation_task()
+    BackgroundTasks.add_task(crud.perform_translation, task.id, request.text, request.languages, db)
+
+    return {"task_id": {task.id}}
